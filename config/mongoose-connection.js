0ef3fa905fab7deba4928433abf189f8b409d415
Rishabh Dev Mishra <rishabhdevmishra388@gmail.com>
2025-09-08 22:34:26 +0530

    New URI

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,14 +1,19 @@
-const mongoose = require("mongoose")
-const config = require("config")
-const debgr = require("debug")("development:mongoose")
+const mongoose = require("mongoose");
+const debgr = require("debug")("development:mongoose");
+require("dotenv").config(); // Load environment variables
 
-mongoose.
-connect(`${config.get("MONGODB_URI")}/scatch`)
-.then(function(){
-    debgr("Connected")
-})
-.catch(function(err){
-    console.log(err);
-})
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose
+    .connect(`${mongoURI}/scatch`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        debgr("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error:", err);
+    });
 
 module.exports = mongoose.connection;
